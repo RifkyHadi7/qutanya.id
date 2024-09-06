9f29b376dc65a5883853a1c70ed72977b75f56b6
albiistafa <izzatalbi@student.ub.ac.id>
2024-09-06 18:56:17 +0700

    page to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { QutanyaLogo } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { Spinner } from "@nextui-org/react";
 
 export default function IndexPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/loginpage"); // Ganti "/loginpage" dengan path halaman login Anda
+    }, 4000); // 4000 milidetik = 4 detik
+
+    return () => clearTimeout(timer); // Bersihkan timer saat komponen unmount
+  }, [router]);
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 mt-32 ">
@@ -11,7 +25,7 @@ export default function IndexPage() {
         </div>
 
         <div className="flex gap-3"></div>
-
+        <Spinner />
         <div className="mt-8"></div>
       </section>
     </DefaultLayout>
