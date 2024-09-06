22e4ee9cdf41f2a8379486ef0cf3552fa6ce894e
NovanM <azizinovanm@gmail.com>
2024-09-06 18:35:49 +0700

    fix : theme color

---
+++ layouts/theme.ts
@@ -0,0 +1,15 @@
+export const lightTheme = {
+    colors: {
+      primary: "#FFFFFF",
+      secondary: "#808585",
+      background: "#25E5DA",
+    },
+  };
+  
+  export const darkTheme = {
+    colors: {
+      primary: "#FFFFFF",
+      secondary: "#808585",
+      background: "#25E5DA",
+    },
+  };
