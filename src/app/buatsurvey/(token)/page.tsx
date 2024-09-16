dee31a0b52a3a6b6a639249281345276706fc0d5
NovanM <azizinovanm@gmail.com>
2024-09-16 09:29:00 +0700

    fix

--- src/app/buatsurvey/(token)/page.tsx
+++ src/app/buatsurvey/(token)/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { Input, Select, SelectItem, Spinner, Button } from "@nextui-org/react";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import DefaultLayout from "@/layouts/default1";
 import { MenuButton } from "@/layouts/menu";
 import { HeaderAvatar } from "@/layouts/headerAvatar";
 import axios from "axios";
-import {  useRouter, useSearchParams } from "next/navigation";
-
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Kategori {
   id: number;
@@ -18,7 +17,7 @@ interface KategoriOption {
   label: string;
 }
 
-export default function BuatSurveyPage() {
+const BuatSurveyComponent = () => {
   const [judulSurvey, setJudulSurvey] = useState("");
   const [linkFormResponden, setLinkFormResponden] = useState("");
   const [linkFormEdit, setLinkFormEdit] = useState("");
@@ -31,12 +30,12 @@ export default function BuatSurveyPage() {
 
   const router = useRouter();
   const param = useSearchParams();
-  const query = param.get('token');
-  
+  const query = param.get("token");
+
   useEffect(() => {
     console.log(query);
     // Mengecek token Google di session storage hanya di sisi klien
-    if (query){
+    if (query) {
       sessionStorage.setItem("googleToken", JSON.stringify(query));
       // Set timeout untuk menghapus data setelah 3600 detik (1 jam)
       document.cookie = `session=${query}; path=/; max-age=3600`; // 1 jam
@@ -150,15 +149,43 @@ export default function BuatSurveyPage() {
 
         {/* Jika tidak ada token Google, tampilkan tombol login */}
         {!googleToken ? (
-          <Button
-            className="w-full mt-4"
-            onClick={() =>
-              (window.location.href =
-                "https://be-qutanya.vercel.app/auth/auth/google")
-            }
-          >
-            Login with Google
-          </Button>
+
+          <div className="flex flex-row w-full mx-auto px-4">
+            <Button
+              className=" mx-auto  lg:w-1/2  bg-slate-200 p-2"
+              onClick={() =>
+                (window.location.href =
+                  "https://be-qutanya.vercel.app/auth/auth/google")
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="40"
+                height="40"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#FFC107"
+                  d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                ></path>
+                <path
+                  fill="#FF3D00"
+                  d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                ></path>
+                <path
+                  fill="#4CAF50"
+                  d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                ></path>
+                <path
+                  fill="#1976D2"
+                  d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                ></path>
+              </svg>
+              Login with Google
+            </Button>
+          </div>
         ) : (
           <section className="flex flex-col gap-4 w-full px-4">
             <div className="flex flex-col gap-4 w-full mx-auto">
@@ -237,4 +264,14 @@ export default function BuatSurveyPage() {
       </section>
     </DefaultLayout>
   );
-}
+};
+
+const BuatSurveyPage = () => {
+  return (
+    <Suspense>
+      <BuatSurveyComponent />
+    </Suspense>
+  );
+};
+
+export default BuatSurveyPage;
